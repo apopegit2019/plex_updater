85b5dc25d1584026c2f4e873193ca52eb4670660
bukarubonzai <44140980+bukarubonzai@users.noreply.github.com>
2019-03-17 12:35:32 -0400

    Update updaterFinal.py
    formatting

--- updaterFinal.py
+++ updaterFinal.py
@@ -10,30 +10,32 @@ def logging(message):  # Simple log file with time/date stamps
         log.write('%s %s \n' % (time.ctime(), message))
 
 
-def version_check(): # decides whether a new version is available.
-    with open('/home/bukarubonzai/plexfiles2/updater/vercur.txt', 'r') as curver: # current version release epoch
-        curvers = int(curver.readline()) # reads the epoch date from the file to compare to the API. Cast to int for compare
+def version_check():  # decides whether a new version is available.
+    with open('/home/bukarubonzai/plexfiles2/updater/vercur.txt', 'r') as curver:  # current version release epoch
+        curvers = int(
+            curver.readline())  # reads the epoch date from the file to compare to the API. Cast to int for compare
     plx_api = 'https://plex.tv/api/downloads/1.json?channel=plexpass'  # plex API returns JSON with downloads
     data = requests.get(plx_api).json()  # pulls in the JSON response from plex
-    nversdate = int(data['computer']['Linux']['release_date']) # pulls the API advertised epoch date for new version. Cast to int for compare
-    nvers = data['computer']['Linux']['version'] # pulls actual version number
-    if nversdate  > curvers: # compares epoch from API to the one for the current version
-        logging('New Version Found %s' % nvers) # adds new version to the log
+    nversdate = int(data['computer']['Linux'][
+                        'release_date'])  # pulls the API advertised epoch date for new version. Cast to int for compare
+    nvers = data['computer']['Linux']['version']  # pulls actual version number
+    if nversdate > curvers:  # compares epoch from API to the one for the current version
+        logging('New Version Found %s' % nvers)  # adds new version to the log
         with open('/home/bukarubonzai/plexfiles2/updater/vercur.txt', 'w') as new:
-            new.write(str(nversdate)) # updating current version with new version epoch date
-        get_download(data) # move to download installation file
+            new.write(str(nversdate))  # updating current version with new version epoch date
+        get_download(data)  # move to download installation file
     else:
         logging('No Update Required.')
         exit()
 
 
-def get_download(data): # grab download installation file
-    installDir = '/home/bukarubonzai/plexfiles2/updater/' # directory where the file will be downloaded
+def get_download(data):  # grab download installation file
+    installDir = '/home/bukarubonzai/plexfiles2/updater/'  # directory where the file will be downloaded
     linux_64 = data['computer']['Linux']['releases'][1]  # specifically nabs the Ubuntu 64 bit part of json response
     print(linux_64)
     download_url = linux_64['url']  # just saving the URL to a variable for simplicity later
     if linux_64['build'] == 'linux-ubuntu-x86_64':  # Checks to make sure I did, in fact, get Ubuntu 64 bit
-        os.chdir(installDir) # move to the installation directory
+        os.chdir(installDir)  # move to the installation directory
         logging('Download Starting: %s' % download_url)
         subprocess.check_call(['wget', download_url])  # downloads current version
         install(download_url, installDir)  # moving to installation
@@ -42,12 +44,11 @@ def get_download(data): # grab download installation file
         exit()
 
 
-
 def install(download_url, installDir):
-    pattern = re.compile(r'(plexmediaserver.*)') # pattern to find file name from download URL
-    fileName = pattern.findall(download_url) # pulls the file name out of the URL
-    filepath = os.path.join(installDir, fileName[0]) # creates the file path to the installation file
-    if os.path.isfile(filepath): # make sure there's a file there
+    pattern = re.compile(r'(plexmediaserver.*)')  # pattern to find file name from download URL
+    fileName = pattern.findall(download_url)  # pulls the file name out of the URL
+    filepath = os.path.join(installDir, fileName[0])  # creates the file path to the installation file
+    if os.path.isfile(filepath):  # make sure there's a file there
         logging('Installation commencing: %s' % filepath)
         try:
             subprocess.check_call(['dpkg', '-i', filepath])  # installs new version
